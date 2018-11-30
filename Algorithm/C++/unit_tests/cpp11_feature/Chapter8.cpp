f2de0b274b62e64d7136cc77f4abdd3388470442
DongChen <352070977@qq.com>
2018-11-30 23:50:10 +0800

    更新

--- Algorithm/C++/unit_tests/cpp11_feature/Chapter8.cpp
+++ Algorithm/C++/unit_tests/cpp11_feature/Chapter8.cpp
@@ -2,3 +2,39 @@
 // Created by ¶­å· on 2018/11/29.
 //
 
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace std;
+
+namespace CPP_11_FEATURE_CHAPTER_8{
+
+    /**
+     * Êý¾Ý¶ÔÆë
+     */
+
+    struct HowManyBytes{
+        char a;
+        int b;
+    };
+
+    TEST(CPP_11_FEATURE, CHAPTER_8_OFFSETOF){
+        cout << sizeof(char) << endl;
+        cout << sizeof(int) << endl;
+        cout << sizeof(HowManyBytes) << endl;
+
+        cout << offsetof(HowManyBytes, a) << endl;
+        cout << offsetof(HowManyBytes, b) << endl;
+    }
+
+    struct alignas(32) ColorVector{
+        double r;
+        double g;
+        double b;
+        double a;
+    };
+
+    TEST(CPP_11_FEATURE, CHAPTER_8_ALIGNOF){
+        cout << alignof(ColorVector) << endl;
+    }
+}
