74465aa558d0e0bffdca759a4a19b5667dbba030
DongChen <352070977@qq.com>
2018-12-07 14:10:12 +0800

    添加Assimp库

---
+++ OpenGL/LearnOpenGL/ReGL/ReGL/src/looper/Scene.h
@@ -0,0 +1,16 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include <assimp/scene.h>
+
+namespace ReGL
+{
+    class Scene
+    {
+    public:
+    private:
+        aiScene * scene_;
+    };
+}
+
+#endif
