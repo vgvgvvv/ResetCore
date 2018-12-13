56fddbea73747bf7f15dd3246c372466805ceed1
DongChen <352070977@qq.com>
2018-12-13 10:52:24 +0800

    更新

---
+++ Unity/CustomMeshManager/Assets/Scripts/CustomGraphic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomGraphic : MaskableGraphic {
+
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        base.OnPopulateMesh(vh);
+    }
+
+
+}
