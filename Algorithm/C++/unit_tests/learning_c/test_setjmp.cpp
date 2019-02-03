ec3f8ad1b26a69de0701f6f36f4501c16e9d843f
DongChen <352070977@qq.com>
2019-02-03 18:05:03 +0800

    更新

---
+++ Algorithm/C++/unit_tests/learning_c/test_setjmp.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 董宸 on 2019/2/3.
+//
+
+#include "gtest/gtest.h"
+#include <setjmp.h>
+#include <stdio.h>
+
+namespace LearningC {
+
+
+    jmp_buf ebuf;
+    //测试long jmp
+    TEST(JMP, TRY){
+        int i;
+        printf("\n");
+        printf("1 ");
+        i = setjmp(ebuf);
+        printf("i = %d\n",i);//注意它不会输出
+        if (i == 0){
+            printf("2 ");
+            longjmp(ebuf, 3);//返回setjmp执行点
+            printf("This will not be printed.");//注意它不会输出
+        }
+        printf("%d", i);
+    }
+}
--- C/LearningC/src/library.c
+++
@@ -1,7 +0,0 @@
-
-#include <stdio.h>
-
-int helloWorld(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
