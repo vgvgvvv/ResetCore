4d4b2b7a6d17282503c4b5b00f25e1dc4deba004
DongChen <352070977@qq.com>
2019-02-23 15:24:18 +0800

    添加Code注入的Demo

---
+++ C#/CodeInject/CodeInject/MethodInjector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace CodeInject
+{
+    public abstract class MethodInjector
+    {
+        public void InjectIL(MethodDefinition methodDefinition)
+        {
+            var processor = methodDefinition.Body.GetILProcessor();
+        }
+    }
+}
