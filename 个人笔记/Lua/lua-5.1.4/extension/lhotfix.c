06a1eb37f8a52199cd0af085d30213180ade9f6b
DongChen <352070977@qq.com>
2019-03-04 00:59:02 +0800

    更新注释

--- 个人笔记/Lua/lua-5.1.4/extension/lhotfix.c
+++ 个人笔记/Lua/lua-5.1.4/extension/lhotfix.c
@@ -6,14 +6,25 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-#define LUA_HOTFIXLIBNAME	"hotswap"
+#define LUA_HOTSWAPLIBNAME	"hotswap"
 
+/**
+ * 初始化热重载地址
+ * @param L
+ * @return
+ */
 static int luaB_hsinit(lua_State* L){
 
     return 1;
 }
 
+/**
+ * 热重载更新
+ * @param L
+ * @return
+ */
 static int luaB_hsupdate(lua_State* L){
+    
     return 1;
 }
 
@@ -25,11 +36,11 @@ static const luaL_Reg hotswap_funcs[] = {
 };
 
 /**
- * 打开热更新库
+ * 打开热重载库
  * @param L
  * @return
  */
 static const int lua_openhotswap(lua_State* L){
-    luaL_register(L, LUA_HOTFIXLIBNAME, hotswap_funcs);
+    luaL_register(L, LUA_HOTSWAPLIBNAME, hotswap_funcs);
     return 1;
 }
