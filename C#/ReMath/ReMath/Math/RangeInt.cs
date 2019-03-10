98e744ac20be2621c35a62c3c80fcb02af57d380
Dong Chen <352070977@qq.com>
2019-03-11 00:49:47 +0800

    更新

---
+++ C#/ReMath/ReMath/Math/RangeInt.cs
@@ -0,0 +1,16 @@
+﻿namespace ReMath
+{
+    public struct RangeInt
+    {
+        public int start;
+        public int length;
+
+        public int end { get { return start + length; } }
+
+        public RangeInt(int start, int length)
+        {
+            this.start = start;
+            this.length = length;
+        }
+    }
+}
---
+++ C#/ReMath/ReMath/Physics/Sphere.cs
@@ -0,0 +1,7 @@
+﻿namespace ReMath
+{
+    public struct Sphere
+    {
+        
+    }
+}
