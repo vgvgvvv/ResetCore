f2b5d2eba7dcfe5dbb8893036b33fa2b9c6fc4ba
Dong Chen <352070977@qq.com>
2019-06-14 00:34:16 +0800

    更新

---
+++ Unity/JoySystem/Assets/Scripts/JobHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+
+public class JobHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyJob job = new MyJob();
+        JobHandle jobHandle = job.Schedule();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/JoySystem/Assets/Scripts/MyJob.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+
+/**
+ * IJob: 执行一次的简单作业
+ * IJobParallelFor: 为一系列值执行的作业
+ * IJobParallelForTransform: 能访问transforms的作业
+ */
+public struct MyJob : IJob
+{
+
+    public void Execute()
+    {
+        
+    }
+}
