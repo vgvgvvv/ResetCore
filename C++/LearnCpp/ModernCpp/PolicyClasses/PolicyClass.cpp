1d581a00d8671c113e484de147d2aa5eaf6c81df
Dong Chen <352070977@qq.com>
2019-11-24 17:18:53 +0800

    剔除_Resharper.caches

---
+++ C++/LearnCpp/ModernCpp/PolicyClasses/PolicyClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 35207 on 2019/11/15 0015.
+//
+
+#include "PolicyClass.h"
---
+++ C++/LearnCpp/ModernCpp/PolicyClasses/PolicyClass.h
@@ -0,0 +1,16 @@
+//
+// Created by 35207 on 2019/11/15 0015.
+//
+
+#ifndef RESETCORE_POLICYCLASS_H
+#define RESETCORE_POLICYCLASS_H
+
+
+
+class PolicyClass {
+
+};
+
+
+
+#endif //RESETCORE_POLICYCLASS_H
