482eca1766a127fcd602daba59f8d4b7a49038b8
Dong Chen <352070977@qq.com>
2020-05-01 23:48:58 +0800

    提交electrondemo工程

---
+++ JavaScript/Electron/electron-test/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
