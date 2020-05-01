2dfd5f8976d71a9f7dabd829266df24c38f4650b
DongChen <352070977@qq.com>
2020-05-02 02:06:42 +0800

    添加swift代码

---
+++ Swift/LearnSwift/LearnSwift/LearnSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by 董宸 on 2020/3/9.
+//  Copyright © 2020 董宸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift/LearnSwift/LearnSwift/LearnSwiftTests/LearnSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  LearnSwiftTests.swift
+//  LearnSwiftTests
+//
+//  Created by 董宸 on 2020/3/9.
+//  Copyright © 2020 董宸. All rights reserved.
+//
+
+import XCTest
+@testable import LearnSwift
+
+class LearnSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
